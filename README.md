c00e42f91859bfbddcfa6ed257fbf0e3e335185f
JesseLD <jesse@DK-ADDIX3.realetech.local>
2024-03-11 13:25:00 -0300

    Initial Project Setup

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# portifolio
-My Portifolio
+#My Portifolio
---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Jessé Oliveira - Software Engineer",
+  description: "Se my work and my thoughts about software engineering.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
